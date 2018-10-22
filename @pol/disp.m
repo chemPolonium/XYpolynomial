c97244a8065657709b032437cd140d44f8fe3b46
chemPolonium <li.boyuan@outlook.com>
2018-10-22 16:34:08 +0800

    怎么这么多注释乱码

--- @pol/disp.m
+++ @pol/disp.m
@@ -1,7 +1,8 @@
 function disp(u)
-% 重载了 Matlab 自带的显示函数
+% 重载了 disp 函数使得可以显示为多项式，虽然这个多项式还是矩阵的形式
+% 可以直接在 Matlab 命令行里输入多项式变量名来显示
 if length(u)==1
-    fprintf('  polynomial\n\n');
+ d   fprintf('  polynomial\n\n');
     disp(u.p);
 else
     fprintf('\n  %d x %d polynomial matrix\n\n',size(u));
