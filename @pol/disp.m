5bb5d36f495e1f49a39d1e2d0d251294ae18e0d0
chemPolonium <li.boyuan@outlook.com>
2018-10-22 16:41:18 +0800

    修复 disp 和 double

--- @pol/disp.m
+++ @pol/disp.m
@@ -2,7 +2,7 @@ function disp(u)
 % 重载了 disp 函数使得可以显示为多项式，虽然这个多项式还是矩阵的形式
 % 可以直接在 Matlab 命令行里输入多项式变量名来显示
 if length(u)==1
- d   fprintf('  polynomial\n\n');
+    fprintf('  polynomial\n\n');
     disp(u.p);
 else
     fprintf('\n  %d x %d polynomial matrix\n\n',size(u));
--- @pol/double.m
+++ @pol/double.m
@@ -1,6 +1,9 @@
 function a=double(u)
 % 将多项式转换为矩阵，只有在矩阵内所有多项式都为常数时可以用
-a=apfunsin(@singledouble,u);
+a=zeros(size(u));
+for i=1:size(u(:))
+    a(i)=singledouble(u(i));
+end
 end
 
 function a=singledouble(u)
