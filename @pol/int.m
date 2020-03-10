a98d5a433a73d7db936566ee6ac671c408ea7af6
chemPolonium <li.boyuan@outlook.com>
2020-03-10 19:44:38 +0800

    optimize int

--- @pol/int.m
+++ @pol/int.m
@@ -13,9 +13,9 @@ end
 function a=singleint(d,u)
 a=u.p;
 if d==1
-    a=pol([a./fliplr((1:size(a,2))),zeros(size(a,1),1)]);
+    a=pol([a./(size(a,2):-1:1),zeros(size(a,1),1)]);
 elseif d==2
-    a=pol([a./flip((1:size(a,1))');zeros(1,size(a,2))]);
+    a=pol([a./((size(a,1):-1:1)');zeros(1,size(a,2))]);
 end
 end
 
