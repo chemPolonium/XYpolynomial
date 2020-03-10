fa0173627b6edd8eeced2177b07d52b1c930853f
chemPolonium <li.boyuan@outlook.com>
2020-03-10 19:38:16 +0800

    use arrayfun in double

--- @pol/double.m
+++ @pol/double.m
@@ -1,9 +1,6 @@
 function a=double(u)
 % 将多项式转换为矩阵，只有在矩阵内所有多项式都为常数时可以用
-a=zeros(size(u));
-for i=1:size(u(:))
-    a(i)=singledouble(u(i));
-end
+a = arrayfun(@singledouble,u);
 end
 
 function a=singledouble(u)
